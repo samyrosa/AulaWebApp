427cdf0c8620634073a958c551436b936c952fc1
samyrosa <sr.oliveira0910@gmail.com>
2023-03-22 11:04:11 -0300

    usuario

---
+++ AulaWebApp/Controllers/UsuariosController.cs
@@ -0,0 +1,17 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+
+namespace AulaWebApp.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
