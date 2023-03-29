b82fa518a69d116df0fe262834da00d2946fa941
samyrosa <sr.oliveira0910@gmail.com>
2023-03-29 09:08:45 -0300

    layout cliente list

---
+++ AulaWebApp/Controllers/ClientesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AulaWebApp.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+    }
+}
