952e6365e2fb7cf1e2b0555fff57cf97d4e937d0
samyrosa <sr.oliveira0910@gmail.com>
2023-03-29 10:48:19 -0300

    cadastro de cliente

--- AulaWebApp/Controllers/ClientesController.cs
+++ AulaWebApp/Controllers/ClientesController.cs
@@ -8,5 +8,37 @@ namespace AulaWebApp.Controllers
         {
             return View();
         }
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(string nome, string telefone)
+        {
+            if (string.IsNullOrEmpty(nome))
+            {
+                TempData["Erro"] = "O campo nome nao pode estar em branco!";
+            }
+            if (string.IsNullOrEmpty(telefone))
+            {
+                TempData["Erro"] = "O campo telefone nao pode estar em branco!";
+            }
+            return View();
+        }
+
+        public IActionResult Editar(int Id)
+        {
+            return View();
+        }
+
+        public IActionResult Excluir(int Id)
+        {
+            return View();
+        }
+        public IActionResult Compras(int Id)
+        {
+            return View();
+        }
     }
 }
